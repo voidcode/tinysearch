b3887c0aa76df92da841f7caa1ba7df98f3c53bb
admin12 <terkelsorensen@gmail.com>
2015-04-04 18:48:48 +0200

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var users = require('./routes/users');
 var index = require('./routes/index');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,10 @@
-$(function(){
-	function newLinksItem(item){
-		return '<div class="item" tabindex="0" data-id="'+item.id+'">
-					<div class="title">'+item.title+'</div>
-					<div class="url">'+item.url+'</div>
-					<div class="short-text">'+item.shorttext+'</div>
-				</div>';
-	}
+function newLinksItem(item){
+		return '<div class="item" tabindex="0" data-id="'+item.id+'">'+
+					'<div class="title">'+item.title+'</div>'+
+					'<div class="url">'+item.url+'</div>'+
+					'<div class="short-text">'+item.shorttext+'</div>'+
+				'</div>';
+}
+$(document).ready(function(){
+	$('select').select2();
 });
